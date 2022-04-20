f923630e7b3537aa27377ad5741c572b39f41fdc
Amani Harrison-Bryan <amanihoot@yahoo.com>
2022-04-19 20:33:06 -0500

    AH4.19v2

---
+++ Project 3321-Group 3/Project 3321-Group 3/LoginMenu.h
@@ -0,0 +1,13 @@
+
+#pragma once
+#include <iostream>
+#include<iomanip>
+#include <ctime>
+#include <string>
+#include <fstream>
+using namespace std;
+
+
+void getLogin();
+
+void validateLogin(string, string);
